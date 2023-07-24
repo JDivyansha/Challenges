a5496fdb0eca654dbc5a9c082e91ebbbf8207ff4
JDivyansha <31919815+JDivyansha@users.noreply.github.com>
2023-07-24 08:52:01 +0530

    Update findkeyvalue.py

--- Challenge3/findkeyvalue.py
+++ Challenge3/findkeyvalue.py
@@ -1,20 +1,20 @@
-def find_value_from_nested_obj(nested_obj, key):
-    if isinstance(nested_obj, dict):
-        if key in nested_obj:
-            return nested_obj[key]
+def find_value_from_nested_obj(obj, key):
+    if isinstance(obj, dict):
+        if key in obj:
+            return obj[key]
         else:
-            for k, v in nested_obj.items():
+            for k, v in obj.items():
                 result = find_value_from_nested_obj(v, key)
                 if result is not None:
                     return result
-    elif isinstance(nested_obj, list):
-        for item in nested_obj:
+    elif isinstance(obj, list):
+        for item in obj:
             result = find_value_from_nested_obj(item, key)
             if result is not None:
                 return result
 
     return None
-nested_obj = {"x": {"y": {"z": "a"}}}
+obj = {"x": {"y": {"z": "a"}}}
 key_to_find =  'y'
-result = find_value_from_nested_obj(nested_obj, key_to_find)
+result = find_value_from_nested_obj(obj, key_to_find)
 print(result)  # Output: value
